ffb782d9890a4340c440f9f1b5be4122316b9509
Christian Serra <castachris@hotmail.it>
2021-12-08 17:01:41 +0100

    feat(aoc2021): :sparkles: [2/2] aoc2021 day 07

---
+++ aoc2021/src/day07/index.ts
@@ -0,0 +1,62 @@
+import run from "aocrunner"
+import _ from 'lodash';
+
+const parseInput = (rawInput: string) => rawInput.split(',').map(s => parseInt(s))
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  const fuelUsedCalculator = (input: number[], position: number): number =>
+    _.sum(input.map(n => Math.abs(n - position))); 
+  
+  return getMinFuelUsed(input, fuelUsedCalculator);
+}
+
+const getMinFuelUsed = (
+  input: number[],
+  fuelUsedCalculator: (input: number[], position: number) => number
+): number => {
+  const max = _.max(input)!!;
+  const min = _.min(input)!!;
+
+  return _.range(min, max).reduce((minFuelUsed, attempt) => {
+    const fuelUsed = fuelUsedCalculator(input, attempt);
+    return Math.min(minFuelUsed, fuelUsed);
+  }, Infinity)
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  
+  const fuelUsageMemo: {[key: number]: number} = {};
+  const fuelUsedCalculator = (input: number[], position: number): number => {
+    return _.sum(input.map(n => {
+      const diff = Math.abs(n - position);
+
+      if (fuelUsageMemo.hasOwnProperty(diff)) 
+        return fuelUsageMemo[diff];
+      
+      const fuelUsage = _.sum(_.range(1, diff+1));
+      fuelUsageMemo[diff] = fuelUsage;
+      return fuelUsage;
+    }));
+  }
+  
+  return getMinFuelUsed(input, fuelUsedCalculator);
+}
+
+run({
+  part1: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // { input: ``, expected: "" },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+})
