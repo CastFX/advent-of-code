63136020710d48a9922cc4026e7a529bfdb875d7
Christian Serra <castachris@hotmail.it>
2021-12-14 09:44:58 +0100

    feat(aoc2021): :sparkles: [2/2] aoc 2021 day 10

--- aoc2021/src/day10/index.ts
+++ aoc2021/src/day10/index.ts
@@ -1,17 +1,78 @@
 import run from "aocrunner"
+import _ from 'lodash'
 
-const parseInput = (rawInput: string) => rawInput
+const parseInput = (rawInput: string) => rawInput.split('\n').map(s => s.split(''))
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  const corruptedScore = input.map(line => corruptingPoints(line))
+  return _.sum(corruptedScore)
+}
+
+const reverseBracket = (bracket: string): string => {
+  switch (bracket) {
+    case '(': return ')'
+    case '[': return ']'
+    case '{': return '}'
+    case '<': return '>'
+    case ')': return '('
+    case ']': return '['
+    case '}': return '{'
+    case '>': return '<'
+    default: return "";
+  }
+}
+const points: {[key: string]: number} = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137,
+};
+const closing = Object.keys(points);
+const opening = closing.map(reverseBracket);
+
+const corruptingPoints = (
+  line: string[],
+  stack: string[] = [],
+): number => {
+  if (_.isEmpty(line)) return 0;
+
+  const firstChar = line[0]
+  const rest = _.drop(line, 1);
+  if (opening.includes(firstChar)) {
+    return corruptingPoints(rest, [firstChar, ...stack]);
+  }
+  if (reverseBracket(firstChar) === stack[0]) {
+    return corruptingPoints(rest, _.drop(stack, 1));
+  }
+  return points[firstChar];
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
+  const incomplete = input
+    .filter(line => corruptingPoints(line) === 0)
+    .map(line => completingPoints(line))
 
-  return
+  const sorted = _.sortBy(incomplete);
+  return sorted[(sorted.length - 1)/ 2];
+}
+
+const completingPoints = (line: string[], stack: string[] = []): number => {
+  if (_.isEmpty(line)) return stack.reduce((points, bracket) => {
+    return points * 5 + opening.indexOf(bracket) + 1
+  }, 0);
+
+  const firstChar = line[0]
+  const rest = _.drop(line, 1);
+  if (opening.includes(firstChar)) {
+    return completingPoints(rest, [firstChar, ...stack]);
+  }
+  if (reverseBracket(firstChar) === stack[0]) {
+    return completingPoints(rest, _.drop(stack, 1));
+  }
+  return -1;
 }
 
 run({
